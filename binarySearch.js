f7ac389301dbd8eb2a24c6d2865236ba581e4e67
Ali ElSheikh <Ali.AElSheikh@outlook.com>
2022-09-02 23:21:02 +0200

    add steps

--- binarySearch.js
+++ binarySearch.js
@@ -1,3 +1,13 @@
+/*
+Let min = 0 and max = n-1.
+If max < min, then stop: target is not present in array. Return -1.
+Compute guess as the average of max and min, rounded down (so that it is an integer).
+If array[guess] equals target, then stop. You found it! Return guess.
+If the guess was too low, that is, array[guess] < target, then set min = guess + 1.
+Otherwise, the guess was too high. Set max = guess - 1.
+Go back to step 2.
+*/
+
 /* Returns either the index of the location in the array,
   or -1 if the array did not contain the targetValue */
 var doSearch = function (array, targetValue) {
